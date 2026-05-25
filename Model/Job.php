c45dc50e7b6a9003e37bbcbf9d62bbd99f4a1a32
Gayan <gayankt@gmail.com>
2026-05-25 13:04:08 +0530

    MAG-2279 | M2.4.9 upgrade - explicit nullable type

--- Model/Job.php
+++ Model/Job.php
@@ -60,8 +60,8 @@ class Job extends AbstractModel
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\App\Config\ConfigResource\ConfigInterface $configInterface,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        ?\Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        ?\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
         array $data = []
     ) {
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
